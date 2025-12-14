ffbbc8e3101d086aa7f46032d6a0bf764c6056e9
Crozingex <48669081+Crozingex@users.noreply.github.com>
2025-12-14 13:11:52 -0600

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'CWRP'
+description 'Placeable Crafting Bench - Fixed OX Item Event'
+version '2.1.0'
+
+client_scripts {
+    'client/client.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/server.lua'
+}
+
+lua54 'yes'
